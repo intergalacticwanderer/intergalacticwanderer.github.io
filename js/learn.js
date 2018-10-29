f4545a0db7be24d348fe1e1a7a70b60d8b04a427
Balint <kolozsvari0611@gmail.com>
2018-10-29 20:59:15 +0200

    js cur valutar etc

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,35 @@
-console.info(50);
+console.info(0);
+
+function sayWelcome() {
+  console.info("Salut " + prenume + " bine ai venit");
+}
+var nume = "Kolozsvari";
+var prenume = "Balint";
+var owner = "Balint";
+var msg1 = "Ai zis Thanos?";
+var msg2 = "Nu, am zis Balint";
+var template = `text cu gilimele " sau simple'`;
+
+var skills = ["html", "css", "js"];
+console.debug("cum te cheama? - " + prenume);
+sayWelcome();
+prenume = "Thanos";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? again? - " + prenume);
+sayWelcome();
+
+// schimb valutar
+var cursEUR = 4.65;
+var myEUR = 10;
+var myRON = 0;
+var friendEUR = 20;
+var friendRON = 0;
+// operatori + - * /
+
+myRON = myEUR * cursEUR;
+console.info("eu am " + myRON + " RON");
+
+friendRON = friendEUR * cursEUR;
+console.info("el are " + friendRON + " RON");
